d374eae2342b1257d551fc98fe75ecba7db47e56
de Souza Junior <Silvio.Souza@unisys.com>
2021-02-22 15:06:01 -0400

    Inicio dia 01

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button color="red">
+        Cliques
+      </Button>
+      <Button color="blue">
+        Cliques
+      </Button>
+      <Button color="green">
+        Cliques
+      </Button>
+    </div>
+  );
+}
+
+export default App;
