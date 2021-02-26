830c4cfb09336b197e9bc8ad0274b984666ad053
SILVIO SOUZA <sjblu7@gmail.com>
2021-02-25 20:16:40 -0400

    Finalizado dia 04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengeContext)
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/SJblu.png" alt="Avatar Silvio Souza" />
@@ -8,7 +11,7 @@ export function Profile() {
                 <strong>Silvio Souza</strong>
                 <p>
                     <img src="icons/level.svg" alt="level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
